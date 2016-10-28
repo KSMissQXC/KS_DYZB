ce59658e5cfb4ef0d360446b24686b42054c5d6a
KSMissQXC <10215655@qq.com>
2016-10-28 10:14:55 +0800

    初始化项目
    1.新建项目

---
+++ KS_DYZB/KS_DYZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KS_DYZB
+//
+//  Created by 耳动人王 on 2016/10/28.
+//  Copyright © 2016年 KS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KS_DYZB/KS_DYZBTests/KS_DYZBTests.swift
@@ -0,0 +1,36 @@
+//
+//  KS_DYZBTests.swift
+//  KS_DYZBTests
+//
+//  Created by 耳动人王 on 2016/10/28.
+//  Copyright © 2016年 KS. All rights reserved.
+//
+
+import XCTest
+@testable import KS_DYZB
+
+class KS_DYZBTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
